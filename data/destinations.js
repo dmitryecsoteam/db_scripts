fae1339a4e042a365d5eb17d6dd122409d94fc1c
Dmitry Kiselev <dmitry.ecsoteam@gmail.com>
2018-10-18 16:24:31 +0300

    PREPROD. Version 1

--- data/destinations.js
+++ data/destinations.js
@@ -2,36 +2,36 @@ db.getCollection('destinations').insertMany([
     {
         _id: NumberInt(1),
         name: ["Osaka", "Осака", "大阪市", "KIX", "OSA", "ITM"],
-        title_en: "Osaka",
-        country: "Japan",
+        name_en: "Osaka",
+        country_en: "Japan",
         iata: "OSA"
     },
     {
         _id: NumberInt(2),
         name: ["Kobe", "Кобе", "Кобэ", "神戸市", "UKB"],
-        title_en: "Kobe",
-        country: "Japan",
+        name_en: "Kobe",
+        country_en: "Japan",
         iata: "UKB"
     },
     {
         _id: NumberInt(3),
         name: ["Nagoya", "Нагоя", "名古屋市", "NGO", "NKM"],
-        title_en: "Nagoya",
-        country: "Japan",
+        name_en: "Nagoya",
+        country_en: "Japan",
         iata: "NGO"
     },
     {
         _id: NumberInt(4),
         name: ["Sapporo", "Саппоро", "札幌市", "SPK", "CTS", "OKD"],
-        title_en: "Sapporo",
-        country: "Japan",
+        name_en: "Sapporo",
+        country_en: "Japan",
         iata: "SPK"
     },
     {
         _id: NumberInt(5),
         name: ["Nagasaki", "Нагасаки", "長崎市", "NGS"],
-        title_en: "Nagasaki",
-        country: "Japan",
+        name_en: "Nagasaki",
+        country_en: "Japan",
         iata: "NGS"
     }
 ])
---
+++ data/test/travels.js
@@ -0,0 +1,9 @@
+db.getCollection('travels').insertMany([
+    {
+        "_id" : ObjectId("5bbce95f81e97acfcd8e0a76"),
+        "origin" : NumberInt(1),
+        "destination" : NumberInt(1),
+        "date" : "2018-10-24",
+        "priceAirplane" : NumberInt(53)
+    }
+]);
